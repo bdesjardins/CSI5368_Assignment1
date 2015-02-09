028788033229cb1d8d1c820d7025f0a142b9aba3
Ben <b-desjardins@live.ca>
2015-02-09 15:56:10 -0500

    Bigram work

--- Assignment1/src/nlp/wordcounter/NLPApplication.java
+++ Assignment1/src/nlp/wordcounter/NLPApplication.java
@@ -15,11 +15,11 @@ public class NLPApplication {
 		System.out.println("2 - Count all words");
 		System.out.println("3 - Count all words (no punctuation)");
 		System.out.println("4 - Count all words (no punctuation or stopwords");
-		//System.out.println("5 - Count all bigrams");
+		System.out.println("5 - Count all bigrams");
 
 		int selected = in.nextInt();
-		while(selected <= 0 && selected > 4){
-			System.out.println("Please select the command to execute (1-4):");
+		while(selected <= 0 && selected > 5){
+			System.out.println("Please select the command to execute (1-5):");
 			in.nextInt();
 		}
 
@@ -35,21 +35,27 @@ public class NLPApplication {
 			break;
 		case 2:
 			input = new File("OutputFiles/tokens.txt");
-			output = new File("OutputFiles/count1.txt");
+			output = new File("OutputFiles/AllCount.txt");
 			reader = new MessageReader(input, output);
 			reader.countWords(false, false);
 			break;
 		case 3:
 			input = new File("OutputFiles/tokens.txt");
-			output = new File("OutputFiles/count2.txt");
+			output = new File("OutputFiles/count_noPunct.txt");
 			reader = new MessageReader(input, output);
 			reader.countWords(true, false);
 			break;
 		case 4:
 			input = new File("OutputFiles/tokens.txt");
-			output = new File("OutputFiles/count3.txt");
+			output = new File("OutputFiles/count_noPunct_noStop.txt");
 			reader = new MessageReader(input, output);
 			reader.countWords(true, true);
+			break;
+		case 5:
+			input = new File("OutputFiles/tokens.txt");
+			output = new File("OutputFiles/bigrams.txt");
+			reader = new MessageReader(input, output);
+			reader.countBigrams(true, true);
 			break;
 		}		
 	}
